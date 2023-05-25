f85b73026d7d628e05c71a618b800411451632cc
Mudpadans <philmodel.101@gmail.com>
2023-05-25 15:03:54 -0400

    adding axios

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -10,9 +10,41 @@ afterEach(async () => {
   await driver.quit();
 });
 
+const renderChoices = async () => {
+  choicesDiv.innerHTML = "";
+  chooseHeader.classList.remove("hide");
+
+  choices.forEach((choice) => {
+    let botHtml = makeRobotChoiceCard(choice);
+    choicesDiv.innerHTML += botHtml;
+  });
+};
+
+const draw = async () => {
+  await driver.findElement(By.id("draw")).click()
+}
+
+const add = async () => {
+  await driver.findElement(By.className('bot-btn')).click()
+}
+
 describe("Duel Duo tests", () => {
   test("page loads with title", async () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+
+  test("draw button displays choices", async () => {
+    await driver.get("http://localhost:8000");
+    await draw()
+    await driver.wait(until.elementLocated(By.id('choices')), 1000 )
+  })
+
+  test('player can add to duo', async () => {
+    await driver.get("http://localhost:8000");
+    await draw()
+    await driver.wait(until.elementLocated(By.id('choices')), 1000 )
+    add()
+    await driver.wait(until.elementLocated(By.id('player-duo')), 1000)
+  })
 });
--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,11 +1,27 @@
-const shuffle = require("../src/shuffle");
+const shuffle = require("../src/shuffle.js");
 
 describe("shuffle should...", () => {
   test("shuffle returns with an array", () => {
-
+    const arr = [1, 2, 3, 4]
+    const result = shuffle(arr)
+    expect(result.length).not.toBe(0)
   })
 
   test('the array has been shuffled', () => {
-    
+    const arr = [1, 2, 3, 4]
+    let count = 0
+    for(let i = 0; i < 50; i++) {
+      const result = shuffle(arr)
+      if(JSON.stringify(result) !== JSON.stringify(arr)) {
+        count++
+      }
+    }
+    expect(count).toBeGreaterThan(25)
+  })
+
+  test('the array has the same amount as the argument', () => {
+    const arr = [1, 2, 3, 4]
+    const result = shuffle(arr)
+    expect(result.length).toEqual(arr.length)
   })
 });
